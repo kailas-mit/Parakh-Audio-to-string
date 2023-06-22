2da07b7d05834b889980a785a53da023e6c31e25
kailas-mit <kailas.mit.dev@.com>
2023-06-22 16:48:27 +0530

    Advance clean code working in firefox as well

--- parakh/settings.py
+++ parakh/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
